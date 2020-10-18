36d80a89e1c624a96832ddf147bd1f8a431e9ae0
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2020-10-18 03:48:29 +0100

    added to bubble, insertion, and merge

--- Sorting.py
+++ Sorting.py
@@ -1,120 +1,166 @@
-# Visualization of Sorting Algorithms
-
-import pygame
+"""This module visualises sorting algorithms using pygame"""
 import random
 import sys
+import time
+import threading
+import math
+import pygame
 
-# def quickSort(numbers):
-#     # https://www.youtube.com/watch?v=MZaf_9IZCrc
-#     if len(numbers) < 2: 
-#         return numbers
+def swap(rects, i, j):
+    """Swap height and position of rectangles"""
+    rects[i].height, rects[j].height = rects[j].height, rects[i].height
+    rects[i].y, rects[j].y = rects[j].y, rects[i].y
 
-#     pivot_index = len(numbers) - 1
+def quick_sort(rects):
+    """Quick sort"""
+    # Explanation of quick sort: https://www.youtube.com/watch?v=MZaf_9IZCrc
+    if len(rects) < 2 or stop_thread:
+        return
 
-#     i = -1
-#     j = 0
-#     while j < pivot_index:
-#         if numbers[j] < numbers[pivot_index]:
-#             i += 1
-#             # swap numbers at i and j
-#             numbers[i], numbers[j] = numbers[j], numbers[i]
-#         j += 1
-    
-#     # swap pivot with number at i + 1
-#     numbers[i + 1], numbers[pivot_index] = numbers[pivot_index], numbers[i + 1]
-#     pivot_index = i + 1
-
-#     # partition left and right side
-#     left = numbers[:pivot_index]
-#     right = numbers[pivot_index + 1:]
-
-#     # Call recursively on left and right side of pivot
-#     return quickSort(left) + [numbers[pivot_index]] + quickSort(right)
-
-def quickSortRect(rectangles):
-    # https://www.youtube.com/watch?v=MZaf_9IZCrc
-    if len(rectangles) < 2: 
-        return rectangles
-
-    pivot_index = len(rectangles) - 1
+    pivot_index = len(rects) - 1
 
     i = -1
     j = 0
     while j < pivot_index:
-        if rectangles[j].height < rectangles[pivot_index].height:
+        if rects[j].height < rects[pivot_index].height:
             i += 1
-            # swap rectangle heights and positions of rectangles at index i and j
-            rectangles[i].height, rectangles[j].height = rectangles[j].height, rectangles[i].height
-            rectangles[i].y, rectangles[j].y = rectangles[j].y, rectangles[i].y
+            swap(rects, i, j)
         j += 1
-    
-    # swap pivot with rectangle at i + 1
-    rectangles[i + 1].height, rectangles[pivot_index].height = rectangles[pivot_index].height, rectangles[i + 1].height
-    rectangles[i + 1].y, rectangles[pivot_index].y = rectangles[pivot_index].y, rectangles[i + 1].y
+    swap(rects, i + 1, pivot_index)
     pivot_index = i + 1
+    time.sleep(TIME_DELAY)
 
     # partition left and right side
-    left = rectangles[:pivot_index]
-    right = rectangles[pivot_index + 1:]
+    left = rects[:pivot_index]
+    right = rects[pivot_index + 1:]
 
     # Call recursively on left and right side of pivot
-    return quickSortRect(left) + [rectangles[pivot_index]] + quickSortRect(right)
-        
+    # No need to return anything since rects inside list are passed by reference even when sliced
+    quick_sort(left)
+    quick_sort(right)
 
-# Make a random set of numbers
-max = 20
-length = 20
-random_numbers = random.choices([i for i in range(max)],k=length)
-print(random_numbers)
-# sorted = quickSort(random_numbers)
-# print(sorted)
+def bubble_sort(rects):
+    """Bubble sort"""
+    for i in range(len(rects) - 1):
+        for j in range(len(rects) - i - 1):
+            if stop_thread:
+                return
+            if rects[j].height > rects[j + 1].height:
+                swap(rects, j, j + 1)
+                time.sleep(TIME_DELAY)
 
-# Pygame setup
-pygame.init()
-clock = pygame.time.Clock()
+def insertion_sort(rects):
+    """Insertion sort"""
+    for i in range(len(rects) - 1):
+        for j in range(i + 1, 0, -1):
+            if stop_thread:
+                return
+            if rects[j].height > rects[j - 1].height:
+                break
+            swap(rects, j, j - 1)
+            time.sleep(TIME_DELAY)
 
-# Setting up thhe main window
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Quick sort algorithm")
+def merge_sort(rects):
+    """Merge sort"""
+    if stop_thread:
+        return
+    length = len(rects)
+    if length > 1:
+        # divide list into 2 if length is more than 1
+        half = math.floor(length / 2)
 
-# Rectangles
-rect_width = screen_width / length
-rect_height = screen_height / 2
-rectangles = []
+        left = rects[:half]
+        right = rects[half:]
 
-for i, number in enumerate(random_numbers):
-    # randomise height
-    rect_height = random.randint(0, screen_height + 1)
-    rect_height =  screen_height * number / max
+        merge_sort(left)
+        merge_sort(right)
 
-    x = i * rect_width
-    y = screen_height - rect_height
-    width = rect_width
-    height = rect_height
+        merge = left + right
+        quick_sort(merge)
 
-    rectangles.append(pygame.Rect(x, y, width, height))
+        time.sleep(TIME_DELAY)
 
-# Colours
-bg_colour = pygame.Color("grey12")
-rect_colour = (200, 200, 200)
 
-while True:
-    # Handling inputs
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
+if __name__ == "__main__":
+    # Pygame setup
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    # Setting up the main window
+    SCREEN_WIDTH = 1500
+    SCREEN_HEIGHT = 800
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Sorting Algorithms")
+
+    # Rectangles
+    QUANTITY = 250 #750
+    TIME_DELAY = 0.01
+    RECT_WIDTH = SCREEN_WIDTH / 2 / QUANTITY
+
+    # Make a list of rectangles with random heights
+    quick_sort_rectangles = []
+    bubble_sort_rectangles = []
+    insertion_sort_rectangles = []
+    merge_sort_rectangles = []
+    for n in range(QUANTITY):
+        # randomise height
+        RECT_HEIGHT =  random.randint(0, SCREEN_HEIGHT / 2 + 1)
+
+        quick_sort_rectangles.append(pygame.Rect(n * RECT_WIDTH,
+                                    SCREEN_HEIGHT / 2 - RECT_HEIGHT,
+                                    RECT_WIDTH,
+                                    RECT_HEIGHT))
+
+        bubble_sort_rectangles.append(pygame.Rect(SCREEN_WIDTH / 2 + n * RECT_WIDTH,
+                                    SCREEN_HEIGHT / 2 - RECT_HEIGHT,
+                                    RECT_WIDTH,
+                                    RECT_HEIGHT))
+
+        insertion_sort_rectangles.append(pygame.Rect(n * RECT_WIDTH,
+                                    SCREEN_HEIGHT - RECT_HEIGHT,
+                                    RECT_WIDTH,
+                                    RECT_HEIGHT))
+
+        merge_sort_rectangles.append(pygame.Rect(SCREEN_WIDTH / 2 + n * RECT_WIDTH,
+                                    SCREEN_HEIGHT - RECT_HEIGHT,
+                                    RECT_WIDTH,
+                                    RECT_HEIGHT))
+
+    # Colours
+    bg_colour = pygame.Color("grey12")
+    rect_colour = (200, 200, 200)
+
     # Algorithm
-    quickSortRect(rectangles)
+    stop_thread = False
+    thread_quick_sort = threading.Thread(target=quick_sort, args=[quick_sort_rectangles])
+    thread_bubble_quick_sort = threading.Thread(target=bubble_sort, args=[bubble_sort_rectangles])
+    thread_insertion_sort = threading.Thread(target=insertion_sort, args=[insertion_sort_rectangles])
+    thread_merge_sort = threading.Thread(target=merge_sort, args=[merge_sort_rectangles])
+    thread_quick_sort.start()
+    thread_bubble_quick_sort.start()
+    thread_insertion_sort.start()
+    thread_merge_sort.start()
 
-    #Visuals
-    screen.fill(bg_colour)
-    for rectangle in rectangles:
-        pygame.draw.rect(screen, rect_colour, rectangle)
+    while True:
+        # Handling inputs
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                stop_thread = True
+                pygame.quit()
+                sys.exit()
 
-    # Update the window
-    pygame.display.flip()
-    clock.tick(60)
+        # Visuals
+        screen.fill(bg_colour)
+        for rectangle in quick_sort_rectangles:
+            pygame.draw.rect(screen, rect_colour, rectangle)
+        for rectangle in bubble_sort_rectangles:
+            pygame.draw.rect(screen, rect_colour, rectangle)
+        for rectangle in insertion_sort_rectangles:
+            pygame.draw.rect(screen, rect_colour, rectangle)
+        for rectangle in merge_sort_rectangles:
+            pygame.draw.rect(screen, rect_colour, rectangle)
+
+
+        # Update the window
+        pygame.display.flip()
+        clock.tick(60)
